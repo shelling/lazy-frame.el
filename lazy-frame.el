9a819ee6569d7a72117b50a6502374440149a295
shelling <navyblueshellingford@gmail.com>
2014-08-23 23:08:35 +0800

    Add Skeleton of lazy-frame.el

---
+++ lazy-frame.el
@@ -0,0 +1,17 @@
+;;; lazy-frame.el --- Lazy Frame Manipulation
+
+;; Copyright (C) 2014 shelling
+;; Author: shelling <navyblueshellingford@gmail.com>
+;; Keyword: frame
+;; Created: 24 Aug 2014
+;; Version: 0.1.0
+;; Package-Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
+
+;;; Code:
+
+
+
+(provide 'lazy-frame)
+
+;;; lazy-frame.el ends here
